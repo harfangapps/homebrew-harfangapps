46cb20354c479e2713b0906c8dfa32c82e4cb015
Martin Angers <martin.n.angers@gmail.com>
2017-07-13 13:17:54 -0400

    regis-companion: add launchd plist

--- Formula/regis-companion.rb
+++ Formula/regis-companion.rb
@@ -19,6 +19,34 @@ class RegisCompanion < Formula
            "-o", bin/"regis-companion", "github.com/harfangapps/regis-companion"
   end
 
+  plist_options :manual => "regis-companion"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/regis-companion</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/regis-companion.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/regis-companion.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
   test do
     system "#{bin}/regis-companion", "--version"
   end
